6358e1ddb52ecf5227e52644e3df59db63a01498
e165715 <e165715@ie.u-ryukyu.ac.jp>
2018-07-18 12:35:43 +0900

    Parallel renewal3

--- java/Parallel/src/main/java/jp/ac/uryukyu/ie/e165715/Main.java
+++ java/Parallel/src/main/java/jp/ac/uryukyu/ie/e165715/Main.java
@@ -11,18 +11,18 @@ public class Main {
 
         int n = 2000;
 
-        Parallel t_arr1a = new Parallel(jp.ac.uryukyu.ie.e165715.Parallel.make_sqrMatrix(n));
-        Parallel t_arr2a = new Parallel(jp.ac.uryukyu.ie.e165715.Parallel.make_sqrMatrix(n));
+        Calculator t_arr1a = new Calculator(jp.ac.uryukyu.ie.e165715.Parallel.make_sqrMatrix(n));
+        Calculator t_arr2a = new Calculator(jp.ac.uryukyu.ie.e165715.Parallel.make_sqrMatrix(n));
 
         int[][] d= new int[n][n];
         //答え入れる用
-        Parallel ans1 = new Parallel(d);
-        Parallel ans2 = new Parallel(d);
+        Calculator ans1 = new Calculator(d);
+        Calculator ans2 = new Calculator(d);
 
         long start = System.currentTimeMillis();
 
         //System.out.println(Parallel.calc_serial(t_arr1bk, t_arr2bk));
-        ans1 = Parallel.calc_serial(t_arr1a, t_arr2a);
+        ans1 = Calculator.calc_serial(t_arr1a, t_arr2a);
         long end = System.currentTimeMillis();
 
 
@@ -30,24 +30,18 @@ public class Main {
         long start2 = System.currentTimeMillis();
 
         //https://qiita.com/koduki/items/086d42b5a3c74ed8b59e#executor-framework
-        ans2 = Parallel.calc_parallel(t_arr1a,t_arr2a);
+        ans2 = Calculator.calc_parallel(t_arr1a,t_arr2a);
         long end2 = System.currentTimeMillis();
         System.out.println(ans1);
         System.out.println(ans2);
 
 
-        long start3 = System.currentTimeMillis();
-        System.out.println(Parallel.calc_serial(t_arr1a, t_arr2a));
-        long end3 = System.currentTimeMillis();
-        long start4 = System.currentTimeMillis();
-        System.out.println(Parallel.calc_parallel(t_arr1a, t_arr2a));
-        long end4 = System.currentTimeMillis();
+
 
 
         System.out.println("逐次" + (end - start)  + "ms");
         System.out.println("並列" + (end2 - start2)  + "ms");
-        System.out.println("逐次" + (end3 - start3)  + "ms");
-        System.out.println("並列" + (end4 - start4)  + "ms");
+
 
     }
 
--- java/Parallel/src/main/java/jp/ac/uryukyu/ie/e165715/Parallel.java
+++ java/Parallel/src/main/java/jp/ac/uryukyu/ie/e165715/Parallel.java
@@ -7,35 +7,7 @@ import java.util.concurrent.Executors;
 
 public class Parallel {
 
-    private int[][] intmatrix;
-    //コンストラクタ
-    public  Parallel(int[][] vector){
-        this.intmatrix = new int[vector.length][vector[0].length];
-        for(int r=0; r<vector.length; r++){
-            for(int c=0; c<vector[r].length; c++){
-                this.intmatrix[r][c] = vector[r][c];
-            }
-        }
 
-    }
-
-    //行列の取得
-    public int[][] getIntArrays(){
-        return this.intmatrix;
-    }
-    //行数を取得
-    public int getIntRow(){
-        return this.intmatrix.length;
-    }
-
-    //列数を取得
-    public int getIntCol(){
-        return this.intmatrix[0].length;
-    }
-
-    private double getIntValue(int row, int col){
-        return this.intmatrix[row][col];
-    }
 
 
     //n行n列のランダム行列を作成
@@ -51,70 +23,8 @@ public class Parallel {
         return d;
 
     }
-    //逐次処理
-    public static Parallel calc_serial(Parallel arr1,Parallel arr2){
-
-            int[][] d = new int[arr1.getIntRow()][arr2.getIntCol()];
-            for (int r = 0; r < arr1.getIntRow(); r++) {
-                for (int c = 0; c < arr2.getIntCol(); c++) {
-                    int sum = 0;
-                    for (int i = 0; i < arr2.getIntRow(); i++) {
-                        sum += arr1.getIntValue(r, i) * arr2.getIntValue(i, c);
-                    }
-                    d[r][c] = sum;
-                }
-            }
-
-            return new Parallel(d);
-
-    }
-    //並列処理（全体）
-    public  static Parallel calc_parallel(Parallel arr1,Parallel arr2){
-        //スレッドの数(固定)のときnewFixedThreadPool
-        //効率的に自動設定する時newWorkStealingPool
-        final int MAX_THREADS = 2000;
-            int[][] d = new int[arr1.getIntRow()][arr2.getIntCol()];
-            Parallel pl = new Parallel(d);
-                ExecutorService es = Executors.newFixedThreadPool(MAX_THREADS);
-                try {
-                    for (int r = 0 ; r < MAX_THREADS; r ++) {
-                        int finalR = r;
-                        es.execute(() -> pl.calc_PPart(arr1, arr2, finalR, d));
-                    }
-                } finally {
-                    es.shutdown();
-
-                }
-
-            return new Parallel(d);
 
 
-    }
-    //並列処理（個別）
-    public  void calc_PPart(Parallel arr1,Parallel arr2,int r,int[][] d){
-        //r行目の処理
-        for(int c=0; c<arr2.getIntCol(); c++){
-            int sum = 0;
-            for(int i=0; i<arr2.getIntRow(); i++){
-                sum += arr1.getIntValue(r, i) * arr2.getIntValue(i, c);
-            }
-            d[r][c] = sum;
-        }
 
-    }
-
-    @Override
-    public String toString(){
-        StringBuilder sb = new StringBuilder();
-        for(int r=0; r<getIntRow(); r++){
-            sb.append("|");
-            for(int c=0; c<getIntCol(); c++){
-                if(intmatrix[r][c] < 0)sb.append(String.format("%d ", intmatrix[r][c]));
-                else sb.append(String.format(" %d ", intmatrix[r][c]));
-            }
-            sb.append("|\n");
-        }
-        return sb.toString();
-    }
 }
 
