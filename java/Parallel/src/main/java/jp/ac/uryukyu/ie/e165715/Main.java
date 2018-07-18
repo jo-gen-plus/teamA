82d2bf7792a837c8d3ecf900784d2913caa0b0f9
e165715 <e165715@ie.u-ryukyu.ac.jp>
2018-07-18 17:12:36 +0900

    completion

--- java/Parallel/src/main/java/jp/ac/uryukyu/ie/e165715/Main.java
+++ java/Parallel/src/main/java/jp/ac/uryukyu/ie/e165715/Main.java
@@ -1,5 +1,7 @@
 package jp.ac.uryukyu.ie.e165715;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) throws InterruptedException {
 
@@ -7,22 +9,26 @@ public class Main {
         //スレッド数はParallelのcalc_parallel内のMAX_THREADSで変更可能
         //スレッド数を自動的に決定する場合はMAX_THREADSをコメントアウトし
         // newFixedThreadPool を　newWorkStealingPool　に変更
-        //Parallel内のmake_sqrMatrixの中のrandnum.nextInt()で行列内の値のランダム幅を変更できる，現在は4(0~4)
+        //Calculator内のmake_sqrMatrixの中のrandnum.nextInt()で行列内の値のランダム幅を変更できる，現在は4(0~4)
 
-        int n = 2000;
+        int n = 500;
 
-        Calculator t_arr1a = new Calculator(jp.ac.uryukyu.ie.e165715.Parallel.make_sqrMatrix(n));
-        Calculator t_arr2a = new Calculator(jp.ac.uryukyu.ie.e165715.Parallel.make_sqrMatrix(n));
+        Matrix t_arr1a = new Matrix(make_sqrMatrix(n));
+        Matrix t_arr2a = new Matrix(make_sqrMatrix(n));
 
         int[][] d= new int[n][n];
         //答え入れる用
-        Calculator ans1 = new Calculator(d);
-        Calculator ans2 = new Calculator(d);
+        Matrix ans1 = new Matrix(d);
+        Matrix ans2 = new Matrix(d);
 
         long start = System.currentTimeMillis();
 
-        //System.out.println(Parallel.calc_serial(t_arr1bk, t_arr2bk));
-        ans1 = Calculator.calc_serial(t_arr1a, t_arr2a);
+        Calculator calc = new Calculator();
+
+
+
+        //System.out.println(Matrix.calc_serial(t_arr1bk, t_arr2bk));
+        ans1 = calc.calc_serial(t_arr1a, t_arr2a);
         long end = System.currentTimeMillis();
 
 
@@ -30,7 +36,7 @@ public class Main {
         long start2 = System.currentTimeMillis();
 
         //https://qiita.com/koduki/items/086d42b5a3c74ed8b59e#executor-framework
-        ans2 = Calculator.calc_parallel(t_arr1a,t_arr2a);
+        ans2 = calc.calc_parallel(t_arr1a,t_arr2a);
         long end2 = System.currentTimeMillis();
         System.out.println(ans1);
         System.out.println(ans2);
@@ -44,5 +50,17 @@ public class Main {
 
 
     }
+    //n行n列のランダム行列を作成
+    public static int[][] make_sqrMatrix(int n){
+        Random randnum = new Random();
+        int[][] d = new int[n][n];
+        for(int i =0 ; i < n ; i ++){
+            for(int j = 0; j< n ; j ++){
+                d[i][j] = randnum.nextInt(4);
+            }
+        }
 
+        return d;
+
+    }
 }
